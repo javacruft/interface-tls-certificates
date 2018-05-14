ff3b27625ab910d71eaf7773af3db493a1d4888b
James Page <james.page@ubuntu.com>
2018-05-14 14:51:44 +0100

    Rework interface for reactive 2.0
    This includes a major change to the state modelling; specifically
    'endpoint.{endpoint_name}.new-request' is set whenever a relation
    data change event is detected for any of the supported keys on
    the relation.
    
    The consuming charm must clear this event at the end of its handler
    invocation for 'endpoint.{endpoint_name}.new-request'.

--- provides.py
+++ provides.py
@@ -1,127 +1,113 @@
 import json
 
-from charms.reactive import hook
-from charms.reactive import scopes
-from charms.reactive import RelationBase
+from charms.reactive import set_flag, clear_flag
+from charms.reactive import Endpoint
+from charms.reactive import when_any, when_not, when
 
 
-class TlsProvides(RelationBase):
+class TlsProvides(Endpoint):
     '''The class that provides a TLS interface other units.'''
-    scope = scopes.UNIT
 
-    @hook('{provides:tls-certificates}-relation-joined')
+    def __init__(self):
+        super(TlsProvides, self).__init__()
+        self.multi_certs = {}
+
+    @when_any('endpoint.{endpoint_name}.changed.common_name',
+              'endpoint.{endpoint_name}.changed.sans',
+              'endpoint.{endpoint_name}.changed.certificate_name',
+              'endpoint.{endpoint_name}.changed.cert_requests')
+    def new_request(self):
+        # New cert request detected, set flags and clear changed flags
+        set_flag(self.expand_name('endpoint.{endpoint_name}.new-request'))
+        clear_flag(self.expand_name('endpoint.{endpoint_name}.'
+                                    'changed.common_name'))
+        clear_flag(self.expand_name('endpoint.{endpoint_name}.'
+                                    'changed.sans'))
+        clear_flag(self.expand_name('endpoint.{endpoint_name}.'
+                                    'changed.certificate_name'))
+        clear_flag(self.expand_name('endpoint.{endpoint_name}.'
+                                    'changed.cert_requests'))
+
+    @when('endpoint.{endpoint_name}.joined')
     def joined(self):
         '''When a unit joins, set the available state.'''
-        # Get the conversation scoped to the unit name.
-        conversation = self.conversation()
-        conversation.set_state('{relation_name}.available')
+        set_flag(self.expand_name('{endpoint_name}.connected'))
 
-    @hook('{provides:tls-certificates}-relation-changed')
-    def changed(self):
-        '''When a unit relation changes, check for a server certificate request
-        and set the server.cert.requested state.'''
-        conversation = self.conversation()
-        cn = conversation.get_remote('common_name')
-        sans = conversation.get_remote('sans')
-        name = conversation.get_remote('certificate_name')
-        requests = conversation.get_remote('cert_requests')
-        # When the relation has all three values set the server.cert.requested.
-        if (cn and sans and name) or requests:
-            conversation.set_state('{relation_name}.server.cert.requested')
-
-    @hook('{provides:tls-certificates}-relation-{broken,departed}')
+    @when_not('endpoint.{endpoint_name}.joined')
     def broken_or_departed(self):
         '''Remove the available state from the unit as we are leaving.'''
-        conversation = self.conversation()
-        conversation.remove_state('{relation_name}.available')
+        clear_flag(self.expand_name('{endpoint_name}.connected'))
 
     def set_ca(self, certificate_authority):
-        '''Set the CA on all the conversations in the relation data.'''
+        '''Set the CA on all relations.'''
         # Iterate over all conversations of this type.
-        for conversation in self.conversations():
+        for relation in self.relations:
             # All the clients get the same CA, so send it to them.
-            conversation.set_remote(data={'ca': certificate_authority})
+            relation.to_publish['ca'] = certificate_authority
 
     def set_chain(self, chain):
         '''Set the chain on all the conversations in the relation data.'''
         # Iterate over all conversations of this type.
-        for conversation in self.conversations():
+        for relation in self.relations:
             # All the clients get the same chain, so send it to them.
-            conversation.set_remote(data={'chain': chain})
+            relation.to_publish['chain'] = chain
 
-    def set_client_cert(self, cert, key):
-        '''Set the client cert and key on the relation data.'''
-        # Iterate over all conversations of this type.
-        for conversation in self.conversations():
-            client = {}
-            client['client.cert'] = cert
-            client['client.key'] = key
-            # Send the client cert and key to the unit using the conversation.
-            conversation.set_remote(data=client)
+    def set_client_cert(self, unit, cert, key):
+        '''Set the client cert and key for the provided units relation.'''
+        unit.relation.to_publish['client.cert'] = cert
+        unit.relation.to_publish['client.key'] = key
 
-    def set_server_cert(self, scope, cert, key):
-        '''Set the server cert and key on the relation data.'''
-        # Get the coversation scoped to the unit.
-        conversation = self.conversation(scope)
-        server = {}
+    def set_server_cert(self, unit, cert, key):
+        '''Set the server cert and key for the provided units relation.'''
+        name = unit.unit_name.replace('/', '_')
+        unit.relation.to_publish['{}.server.cert'.format(name)] = cert
+        unit.relation.to_publish['{}.server.key'.format(name)] = key
+
+    def set_server_multicerts(self, unit):
+        '''Provide the processed requests for the provided unit scope.'''
         # The scope is the unit name, replace the slash with underscore.
-        name = scope.replace('/', '_')
-        # Prefix the key with name so each unit can get a unique cert and key.
-        server['{0}.server.cert'.format(name)] = cert
-        server['{0}.server.key'.format(name)] = key
-        # Send the server cert and key to the unit using the conversation.
-        conversation.set_remote(data=server)
-        # Remove the server.cert.requested state as it is no longer needed.
-        conversation.remove_state('{relation_name}.server.cert.requested')
+        name = unit.unit_name.replace('/', '_')
+        unit.relation.to_publish['{}.processed_requests'.format(name)] = \
+            json.dumps(self.multi_certs[unit.unit_name], sort_keys=True)
 
-    def set_server_multicerts(self, scope):
-        # The scope is the unit name, replace the slash with underscore.
-        name = scope.replace('/', '_')
-        conversation = self.conversation(scope)
-        multi_certs = conversation.get_local('multi_certs')
-        conversation.set_remote(
-            '{}.processed_requests'.format(name),
-            json.dumps(multi_certs, sort_keys=True))
-        conversation.remove_state('{relation_name}.server.cert.requested')
-
-    def add_server_cert(self, scope, cn, cert, key):
+    def add_server_cert(self, unit, cn, cert, key):
         '''
             'client_0': {
                 'admin': {
                     'cert': cert
                     'key': key}}
         '''
-        conversation = self.conversation(scope)
-        multi_certs = conversation.get_local('multi_certs')
-        if multi_certs:
-            multi_certs[cn] = {
-                'cert': cert,
-                'key': key}
+        if unit.unit_name not in self.multi_certs:
+            self.multi_certs[unit.unit_name] = {}
 
-        else:
-            multi_certs = {
-                cn: {
-                    'cert': cert,
-                    'key': key}}
-
-        conversation.set_local('multi_certs', multi_certs)
+        self.multi_certs[unit.unit_name][cn] = {
+            'cert': cert,
+            'key': key
+        }
 
     def get_server_requests(self):
         '''One provider can have many requests to generate server certificates.
         Return a map of all server request objects indexed by the scope
         which is essentially unit name.'''
         request_map = {}
-        for conversation in self.conversations():
-            scope = conversation.scope
-            request = {}
-            request['common_name'] = conversation.get_remote('common_name')
-            sans = conversation.get_remote('sans')
-            if sans:
-                request['sans'] = json.loads(sans)
-            request['certificate_name'] = conversation.get_remote('certificate_name')  # noqa
-            cert_requests = conversation.get_remote('cert_requests')
-            if cert_requests:
-                request['cert_requests'] = json.loads(cert_requests)
-            # Create a map indexed by scope.
-            request_map[scope] = request
+        for relation in self.relations:
+            for unit in relation.units:
+                common_name = unit.received['common_name']
+                sans = unit.received['sans']
+                certificate_name = unit.received['certificate_name']
+                cert_requests = unit.received['cert_requests']
+                if (not all([common_name, certificate_name]) or
+                        not cert_requests):
+                    continue
+                request = {
+                    'unit': unit,
+                    'common_name': common_name,
+                    'certificate_name': certificate_name,
+                }
+                if sans:
+                    request['sans'] = json.loads(sans)
+                if cert_requests:
+                    request['cert_requests'] = json.loads(cert_requests)
+            # Create a map indexed by unit name.
+            request_map[unit.unit_name] = request
         return request_map
